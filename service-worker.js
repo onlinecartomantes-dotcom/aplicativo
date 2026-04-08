50aa6eecd43ed98252a361401cc3d4159d0bcc51
onlinecartomantes-dotcom <onlinecartomantes@gmail.com>
2026-04-08 19:22:03 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "cartomantes-v1";
+
+const urlsToCache = [
+  "/aplicativo/",
+  "/aplicativo/index.html",
+  "/aplicativo/logo-192.png",
+  "/aplicativo/logo-512.png"
+];
+
+// instalar
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ativar
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// fetch (offline + cache inteligente)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
